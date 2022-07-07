4146d2a80a9136835be2a568b50d658dff8cc5e8
Stepanova Valeriya <shestakovalera200@mail.ru>
2022-07-07 20:52:45 +0300

    Footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="wrapper">
     <Header></Header>
+    <main class="page"></main>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from './app/Header.vue';
+import Footer from './app/Footer.vue';
 export default {
   data() {
     return {};
   },
   components: {
     Header,
+    Footer,
   },
 };
 </script>
@@ -19,9 +23,12 @@ export default {
 <style lang="scss">
 @import 'materialize-css/dist/css/materialize.min.css';
 .wrapper {
-  min-height: 100%;
+  min-height: 100vh;
   overflow: hidden;
   display: flex;
   flex-direction: column;
+}
+.page {
+  flex: 1 1 auto;
 }
 </style>
---
+++ src/app/Footer.vue
@@ -0,0 +1,77 @@
+<template>
+  <footer class="footer__container _container">
+    <div class="footer__body">
+      <div class="footer__contacts">
+        <span>#visitsedona</span>
+        <span>+7 800 555-86-28</span>
+      </div>
+      <ul class="footer__social social">
+        <li class="social__item">
+          <a class="social__link" href="#"
+            ><img src="../assets/img/footer/fb.png" alt="fb"
+          /></a>
+        </li>
+        <li class="social__item">
+          <a class="social__link" href="#"
+            ><img src="../assets/img/footer/twitter.png" alt="twitter"
+          /></a>
+        </li>
+        <li class="social__item">
+          <a class="social__link" href="#"
+            ><img src="../assets/img/footer/youtube.png" alt="youtube"
+          /></a>
+        </li>
+      </ul>
+      <div class="footer__info">
+        <span>website by</span>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+  &__container {
+    width: 100%;
+    padding: 36px 10px;
+    @include breakpoint(sm) {
+      padding: 16px 10px;
+    }
+  }
+  &__body {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    @include breakpoint(sm) {
+      flex-direction: column;
+      gap: 5px;
+    }
+  }
+  &__contacts {
+    font-weight: 700;
+    font-size: 21px;
+    line-height: 26px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 9px;
+    @include breakpoint(md) {
+      font-size: 16px;
+      line-height: 16px;
+    }
+  }
+  &__info {
+    line-height: 26px;
+  }
+}
+.social {
+  display: flex;
+  gap: 6px;
+
+  &__item {
+  }
+
+  &__link {
+  }
+}
+</style>
