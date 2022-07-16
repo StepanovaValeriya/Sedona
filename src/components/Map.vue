9b878d7db6e1b74304c32152a0ac0fa1ddb3ba90
DESKTOP-RAJHEII\Валерия <shestakovalera200@mail.ru>
2022-07-16 21:55:01 +0300

    Блок с картой

---
+++ src/components/Map.vue
@@ -0,0 +1,29 @@
+<!-- @format -->
+
+<template>
+  <section class="page__map map">
+    <div class="map__container _container">
+      <div class="map__image _ibg">
+        <img src="../assets/img/map.jpg" alt="map" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.page {
+  &__map {
+  }
+}
+.map {
+  &__container {
+  }
+
+  &__image {
+    padding: 0 0 40% 0;
+    @include breakpoint(sm) {
+      padding: 0 0 60% 0;
+    }
+  }
+}
+</style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,15 +2,17 @@
   <Main></Main>
   <Features></Features>
   <ButtonSearch></ButtonSearch>
+  <Map></Map>
 </template>
 
 <script>
 import Main from '../components/Main.vue';
 import Features from '../components/Features.vue';
 import ButtonSearch from '../components/ButtonSearch.vue'
+import Map from '../components/Map.vue'
 
 export default {
   name: 'index',
-  components: { Main, Features, ButtonSearch },
+  components: { Main, Features, ButtonSearch, Map },
 };
 </script>
